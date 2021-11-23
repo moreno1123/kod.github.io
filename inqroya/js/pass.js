79c7b72b12f5ff9d4713487fdbe50e2543d29855
Moreno Franjković <83433432+moreno1123@users.noreply.github.com>
2021-11-23 15:59:03 +0100

    Create pass.js

---
+++ inqroya/js/pass.js
@@ -0,0 +1,9 @@
+
+var password;
+var pass = "12345";
+password = prompt('Lozika:', ' ');
+if (password == pass){
+  alert('Nastavi')
+}else{
+  window.location = "https://www.google.com/search?q=klokan&sxsrf=AOaemvLBdsR0tv47jL4RaqYMBjuvGXaH4w:1637679304270&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiT8b7p3q70AhUUv4sKHY61CCkQ_AUoAXoECAEQAw&biw=1920&bih=973&dpr=1#imgrc=4ngkXmP0xJ9gTM";
+}
